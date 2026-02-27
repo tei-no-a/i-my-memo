5cb28aa02ea1877cb6c9b42f190320189bb79a8d
Diakg <hishinonono@gmail.com>
2026-02-27 16:51:32 +0900

    キーバインド変更

--- src/constants/keybindings.ts
+++ src/constants/keybindings.ts
@@ -2,6 +2,6 @@ import type { KeyBinding } from '../types/keybindings';
 
 export const DEFAULT_KEYBINDINGS: KeyBinding[] = [
     { key: 'Enter', ctrlKey: true, action: 'createMemo' },
-    { key: 'm', ctrlKey: true, action: 'exportMemo' },
+    { key: 'Enter', altKey: true, action: 'exportMemo' },
     { key: 'd', ctrlKey: true, action: 'deleteMemo' },
 ];
